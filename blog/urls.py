4c495cac245947a613d016772318200d9eded14f
Alon Belkin <alon.belkin.2006@gmail.com>
2020-01-05 18:49:19 +0200

    initial commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from .views import BlogListView, BlogDetailView
 
 
 urlpatterns = [
-    path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
 ]
